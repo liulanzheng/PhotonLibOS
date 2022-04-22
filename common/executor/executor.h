8084b7b91eb73be4e4446216fd4e170d867db7b7
鸣舟 <ray.dr@alibaba-inc.com>
2022-04-22 18:14:33 +0800

    helper structs and functions goes into `Executor` interface class

--- common/executor/executor.h
+++ common/executor/executor.h
@@ -6,57 +6,7 @@
 #include <atomic>
 #include <type_traits>
 
-#include "photon/common/executor/executor.h"
-
 namespace Executor {
-
-constexpr int64_t kCondWaitMaxTime = 1000L * 1000;
-
-struct void_t {};
-template <typename T>
-struct no_void {
-    using type =
-        typename std::conditional<std::is_void<T>::value, void_t, T>::type;
-};
-
-template <typename Func, typename R = typename std::result_of<Func()>::type>
-auto _no_void_ret_func_helper(Func &&func) ->
-    typename std::enable_if<std::is_void<R>::value, void_t>::type {
-    func();
-    return {};
-}
-
-template <typename Func, typename R = typename std::result_of<Func()>::type>
-auto _no_void_ret_func_helper(Func &&func) ->
-    typename std::enable_if<!std::is_void<R>::value, R>::type {
-    return func();
-}
-
-template <typename R, typename Context>
-struct AsyncResult {
-    typename no_void<R>::type result;
-    int err = 0;
-    std::atomic_bool gotit;
-    typename Context::Mutex mtx;
-    typename Context::Cond cond;
-    AsyncResult() : gotit(false), cond(mtx) {}
-    typename no_void<R>::type wait_for_result() {
-        typename Context::CondLock lock(mtx);
-        while (!gotit.load(std::memory_order_acquire)) {
-            cond.wait_for(lock, kCondWaitMaxTime);
-        }
-        if (err) errno = err;
-        return std::move(result);
-    }
-    template <typename T>
-    void done(T &&t) {
-        typename Context::CondLock lock(mtx);
-        result = std::forward<T>(t);
-        err = errno;
-        gotit.store(true, std::memory_order_release);
-        cond.notify_all();
-    }
-};
 
 class ExecutorImpl;
 
@@ -65,6 +15,29 @@ void delete_ease_executor(ExecutorImpl *e);
 void issue(ExecutorImpl *e, Callback<> cb);
 
 class Executor {
+protected:
+    static constexpr int64_t kCondWaitMaxTime = 1000L * 1000;
+
+    struct void_t {};
+    template <typename T>
+    struct no_void {
+        using type =
+            typename std::conditional<std::is_void<T>::value, void_t, T>::type;
+    };
+
+    template <typename Func, typename R = typename std::result_of<Func()>::type>
+    static auto _no_void_ret_func_helper(Func &&func) ->
+        typename std::enable_if<std::is_void<R>::value, void_t>::type {
+        func();
+        return {};
+    }
+
+    template <typename Func, typename R = typename std::result_of<Func()>::type>
+    static auto _no_void_ret_func_helper(Func &&func) ->
+        typename std::enable_if<!std::is_void<R>::value, R>::type {
+        return func();
+    }
+
 public:
     ExecutorImpl *e;
     Executor() : e(new_ease_executor()) {}
@@ -73,7 +46,29 @@ public:
     template <typename Context = StdContext, typename Func,
               typename R = typename std::result_of<Func()>::type>
     typename no_void<R>::type perform(Func &&act) {
-        AsyncResult<R, Context> aret;
+        struct AsyncResult {
+            typename no_void<R>::type result;
+            int err = 0;
+            std::atomic_bool gotit;
+            typename Context::Mutex mtx;
+            typename Context::Cond cond;
+            AsyncResult() : gotit(false), cond(mtx) {}
+            typename no_void<R>::type wait_for_result() {
+                typename Context::CondLock lock(mtx);
+                while (!gotit.load(std::memory_order_acquire)) {
+                    cond.wait_for(lock, kCondWaitMaxTime);
+                }
+                if (err) errno = err;
+                return std::move(result);
+            }
+            void done(typename no_void<R>::type &&t) {
+                typename Context::CondLock lock(mtx);
+                result = std::move(t);
+                err = errno;
+                gotit.store(true, std::memory_order_release);
+                cond.notify_all();
+            }
+        } aret;
         auto work = [act, &aret] {
             if (!aret.gotit.load(std::memory_order_acquire)) {
                 aret.done(_no_void_ret_func_helper(act));
