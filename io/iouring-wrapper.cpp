4bdb180c620fcbc2d7e17e16578464bd3ee25f74
chenbo <chenbo.chen@alibaba-inc.com>
2022-06-01 14:09:25 +0800

    setrlimit for iouring

--- io/iouring-wrapper.cpp
+++ io/iouring-wrapper.cpp
@@ -18,6 +18,7 @@ limitations under the License.
 
 #include <unistd.h>
 #include <sys/eventfd.h>
+#include <sys/resource.h>
 #include <fcntl.h>
 #include <cstdint>
 #include <limits>
@@ -68,6 +69,11 @@ public:
     }
 
     int init() {
+        rlimit resource_limit{.rlim_cur = RLIM_INFINITY, .rlim_max = RLIM_INFINITY};
+        if (setrlimit(RLIMIT_MEMLOCK, &resource_limit) != 0) {
+            LOG_WARN("iouring: current user has no permission to set unlimited RLIMIT_MEMLOCK, change to root?");
+        }
+
         m_ring = new io_uring{};
         io_uring_params params{};
         int ret = io_uring_queue_init_params(QUEUE_DEPTH, m_ring, &params);
