e4cfcad141b757aabccaf7cf0b2b67578b90ae87
huiba.lhb <huiba.lhb@alibaba-inc.com>
2022-11-22 21:21:34 +0800

    redefine UDP interface

--- net/datagram_socket.h
+++ net/datagram_socket.h
@@ -24,12 +24,9 @@ namespace net {
 
 class IDatagramSocket : public IMessage, public ISocketBase, public ISocketName {
 public:
-    virtual int connect(const void* addr) = 0;
-    virtual int bind(const void* addr) = 0;
-    int connect(const char* addr)   { return connect((void*)addr); }
-    int connect(EndPoint ep)        { return connect(&ep); }
-    int bind(const char* addr)      { return bind((void*)addr); }
-    int bind(EndPoint ep)           { return bind(&ep); }
+    struct Addr;
+    virtual int connect(const Addr* addr) = 0;
+    virtual int bind(const Addr* addr) = 0;
 
     ssize_t send(const struct iovec* iov, int iovcnt, int flags = 0) {
         return IMessage::send(iov, iovcnt, nullptr, flags);
@@ -37,23 +34,6 @@ public:
     ssize_t send(const void* buf, size_t count, int flags = 0) {
         return IMessage::send(buf, count, nullptr, flags);
     }
-    ssize_t sendto(const struct iovec* iov, int iovcnt,
-                               EndPoint to, int flags = 0) {
-        return IMessage::sendto(iov, iovcnt, &to, flags);
-    }
-    ssize_t sendto(const void* buf, size_t count,
-                       EndPoint to, int flags = 0) {
-        return IMessage::sendto(buf, count, &to, flags);
-    }
-    ssize_t sendto(const struct iovec* iov, int iovcnt,
-                            const char* to, int flags = 0) {
-        return IMessage::sendto(iov, iovcnt, to, flags);
-    }
-    ssize_t sendto(const void* buf, size_t count,
-                    const char* to, int flags = 0) {
-        return IMessage::sendto(buf, count, to, flags);
-    }
-
     ssize_t recv(const struct iovec* iov, int iovcnt, int flags = 0) {
         return IMessage::recv(iov, iovcnt, nullptr, flags);
     }
@@ -61,35 +41,65 @@ public:
         iovec v {buf, count};
         return IMessage::recv(&v, 1, nullptr, flags);
     }
-    // if from_addr != nullptr, *(int*)from_addr MUST be sizeof(*from_addr)
-    ssize_t recvfrom(const struct iovec* iov, int iovcnt,
-                              EndPoint* from, int flags = 0) {
-        static_assert(sizeof(*from) >= sizeof(int));
-        *(int*)from = sizeof(*from);
-        return IMessage::recvfrom(iov, iovcnt, from, flags);
-    }
-    ssize_t recvfrom(void* buf, size_t count,
-                EndPoint* from, int flags = 0) {
-        static_assert(sizeof(*from) >= sizeof(int));
-        *(int*)from = sizeof(*from);
-        return IMessage::recvfrom(buf, count, from, flags);
-    }
-    ssize_t recvfrom(const struct iovec* iov, int iovcnt,
-                     char* from, int addrlen, int flags = 0) {
-        assert(addrlen >= sizeof(int));
-        *(int*)from = addrlen;
-        return IMessage::recvfrom(iov, iovcnt, from, flags);
-    }
-    ssize_t recvfrom(void* buf, size_t count,
-                    char* from, int addrlen, int flags = 0) {
+    using IMessage::recvfrom;
+    template<typename B, typename S, typename A>
+    ssize_t recvfrom(B* buf, S count, A* from, int addrlen, int flags = 0) {
         assert(addrlen >= sizeof(int));
         *(int*)from = addrlen;
         return IMessage::recvfrom(buf, count, from, flags);
     }
 };
 
-IDatagramSocket* new_udp_socket();
-IDatagramSocket* new_uds_datagram_socket();
+class UDPSocket : public IDatagramSocket {
+public:
+    using base = IDatagramSocket;
+    int connect(EndPoint ep) { return base::connect((Addr*)&ep); }
+    int bind(EndPoint ep) { return base::bind((Addr*)&ep); }
+    ssize_t sendto(const struct iovec* iov, int iovcnt,
+                               EndPoint to, int flags = 0) {
+        return base::sendto(iov, iovcnt, &to, flags);
+    }
+    ssize_t sendto(const void* buf, size_t count,
+                       EndPoint to, int flags = 0) {
+        return base::sendto(buf, count, &to, flags);
+    }
+    // if from_addr != nullptr, *(int*)from_addr MUST be sizeof(*from_addr)
+    ssize_t recvfrom(const struct iovec* iov, int iovcnt,
+                              EndPoint* from, int flags = 0) {
+        return base::recvfrom(iov, iovcnt, from, sizeof(*from), flags);
+    }
+    ssize_t recvfrom(void* buf, size_t count,
+                EndPoint* from, int flags = 0) {
+        return base::recvfrom(buf, count, from, sizeof(*from), flags);
+    }
+};
+
+class UDS_DatagramSocket : public IDatagramSocket {
+public:
+    using base = IDatagramSocket;
+    int connect(const char* addr) { return base::connect((Addr*)addr); }
+    int bind(const char* addr) { return base::bind((Addr*)addr); }
+    ssize_t sendto(const struct iovec* iov, int iovcnt,
+                            const char* to, int flags = 0) {
+        return base::sendto(iov, iovcnt, (Addr*)to, flags);
+    }
+    ssize_t sendto(const void* buf, size_t count,
+                    const char* to, int flags = 0) {
+        return base::sendto(buf, count, (Addr*)to, flags);
+    }
+    ssize_t recvfrom(const struct iovec* iov, int iovcnt,
+                     char* from, int addrlen, int flags = 0) {
+        return base::recvfrom(iov, iovcnt, from, addrlen, flags);
+    }
+    ssize_t recvfrom(void* buf, size_t count,
+                    char* from, int addrlen, int flags = 0) {
+        return base::recvfrom(buf, count, from, addrlen, flags);
+    }
+};
+
+UDPSocket* new_udp_socket();
+
+UDS_DatagramSocket* new_uds_datagram_socket();
 
 }  // namespace net
 }  // namespace photon
