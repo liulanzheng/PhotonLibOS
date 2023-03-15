5717bf0513f549ec75f1154b4c7681254010f2e4
chenbo <chenbo.chen@alibaba-inc.com>
2023-03-15 14:54:11 +0800

    fix cmake

--- CMake/Finduring.cmake
+++ CMake/Finduring.cmake
@@ -1,6 +1,6 @@
 # * Find liburing Find the liburing library and includes
 #
-# URING_INCLUDE_DIR - where to find cma.h, etc. URING_LIBRARIES - List of
+# URING_INCLUDE_DIR - where to find liburing.h, etc. URING_LIBRARIES - List of
 # libraries when using uring. URING_FOUND - True if uring found.
 
 find_path(URING_INCLUDE_DIR liburing.h)
@@ -11,14 +11,13 @@ find_package_handle_standard_args(uring DEFAULT_MSG URING_LIBRARIES
                                   URING_INCLUDE_DIR)
 
 if(URING_FOUND)
-    if(NOT TARGET URING::uring)
-        add_library(URING::uring UNKNOWN IMPORTED)
+    if(NOT TARGET uring)
+        add_library(uring UNKNOWN IMPORTED)
     endif()
     set_target_properties(
-        URING::uring
+        uring
         PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${URING_INCLUDE_DIR}"
                    IMPORTED_LINK_INTERFACE_LANGUAGES "C"
                    IMPORTED_LOCATION "${URING_LIBRARIES}")
+    mark_as_advanced(URING_LIBRARIES)
 endif()
-
-mark_as_advanced(URING_LIBRARIES)
