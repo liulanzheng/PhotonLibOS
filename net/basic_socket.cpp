269f9c20cea98b3ffa8898afca28b08b822629f1
鸣舟 <ray.dr@alibaba-inc.com>
2023-04-28 17:36:44 +0800

    do not use attribute with lambda return type mark

--- net/basic_socket.cpp
+++ net/basic_socket.cpp
@@ -237,16 +237,16 @@ ssize_t sendfile_fallback(ISocketStream* out_stream,
             int in_fd, off_t offset, size_t count, uint64_t timeout) {
     char buf[64 * 1024];
     void* ptr_unused = nullptr;
-    auto func = [&]() __attribute__((always_inline)) -> ssize_t {
+    auto func = [&]() __attribute__((always_inline)) {
         size_t s = sizeof(buf);
         if (s > count) s = count;
         ssize_t n_read = ::pread(in_fd, buf, s, offset);
         if (n_read != (ssize_t) s)
-            LOG_ERRNO_RETURN(0, -1, "failed to read fd ", in_fd);
+            LOG_ERRNO_RETURN(0, (ssize_t)-1, "failed to read fd ", in_fd);
         offset += n_read;
         ssize_t n_write = out_stream->write(buf, s);
         if (n_write != (ssize_t) s)
-            LOG_ERRNO_RETURN(0, -1, "failed to write to stream ", out_stream);
+            LOG_ERRNO_RETURN(0, (ssize_t)-1, "failed to write to stream ", out_stream);
         return n_write;
     };
     return doio_n(ptr_unused, count, func);
