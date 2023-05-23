2d783f8e4cc1997ffc12199b1637714e89964898
yuchen.cc <yuchen.cc@alibaba-inc.com>
2023-05-24 00:35:36 +0800

    add hints for finde2fs.cmake

--- CMake/Finde2fs.cmake
+++ CMake/Finde2fs.cmake
@@ -1,6 +1,10 @@
-find_path(LIBE2FS_INCLUDE_DIRS ext2fs/ext2fs.h)
-
-find_library(LIBE2FS_LIBRARIES ext2fs)
+if (LIBE2FS_PATH)
+    find_path(LIBE2FS_INCLUDE_DIRS ext2fs/ext2fs.h HINTS ${LIBE2FS_PATH}/include)
+    find_library(LIBE2FS_LIBRARIES ext2fs HINTS ${LIBE2FS_PATH}/lib)
+else()
+    find_path(LIBE2FS_INCLUDE_DIRS ext2fs/ext2fs.h)
+    find_library(LIBE2FS_LIBRARIES ext2fs)
+endif()
 
 find_package_handle_standard_args(e2fs DEFAULT_MSG LIBE2FS_LIBRARIES LIBE2FS_INCLUDE_DIRS)
 
